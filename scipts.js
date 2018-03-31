67bfdb0f001e0a688ee200b930769246e6dd8efd
Serj Telepov <serjtelepov@gmail.com>
2018-03-31 21:09:04 +0300

    + calendar task

--- scipts.js
+++ scipts.js
@@ -5369,9 +5369,7 @@ addLi();
 
 // Создайте дерево из объекта
 // todo
-
-
-
+/*
 var data = {
   "Рыбы": {
     "Форель": {},
@@ -5389,3 +5387,80 @@ var data = {
     }
   }
 };
+function createTree(container, obj) {
+  container.appendChild(createTreeDom(obj));
+}
+
+function createTreeDom(obj) {
+  // если нет детей, то рекурсивный вызов ничего не возвращает
+  // так что вложенный UL не будет создан
+  // if (isObjectEmpty(obj)) return;
+
+  var ul = document.createElement('ul');
+
+  for (var key in obj) {
+    var li = document.createElement('li');
+    li.innerHTML = key; // 1 Рыбы 2 Форель
+
+    var childrenUl = createTreeDom(obj[key]); // обж Фищ, {}, 
+    if (childrenUl) li.appendChild(childrenUl);
+
+    ul.appendChild(li);
+  }
+
+  return ul;
+}
+
+function isObjectEmpty(obj) {
+  for (var key in obj) {
+    return false;
+  }
+  return true;
+};
+
+var container = document.getElementById('container');
+createTree(container, data); // создаёт
+*/
+
+// Календарь
+/*
+function createCalendar(id, year, month) {
+  var elem = document.getElementById(id);
+
+  var mon = month - 1;
+  var d = new Date(year, mon);
+
+  var table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+  for (var i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+
+    if (getDay(d) % 7 == 6) {
+      table += '</tr><tr>';
+    }
+
+    d.setDate(d.getDate() + 1);
+  }
+
+  if (getDay(d) != 0) {
+    for (var i = getDay(d); i < 7; i++) {
+      table += '<td></td>';
+    }
+  }
+
+  table += '</tr></table>';
+  elem.innerHTML = table;
+}
+
+function getDay(date) {
+  var day = date.getDay();
+  if (day == 0) day = 7;
+  return day - 1;
+}
+
+createCalendar("calendar", 2012, 9)
+*/
