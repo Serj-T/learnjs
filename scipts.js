9f419304985bd0c7eba50f9f9828f002fa5bb11b
Serj Telepov <serjtelepov@gmail.com>
2018-04-16 20:03:45 +0300

    +carousel

--- scipts.js
+++ scipts.js
@@ -5986,3 +5986,24 @@ for (let i = 0; i < pane.length; i++) {
   });
 };
 */
+
+// Карусель
+/*
+var width = 130; // ширина изображения
+var count = 3; // количество изображений
+var carousel = document.getElementById('carousel');
+var list = carousel.querySelector('ul');
+var listElems = carousel.querySelectorAll('li');
+var position = 0; // текущий сдвиг влево
+
+carousel.querySelector('.prev').onclick = function() {
+  position = Math.min(position + width * count, 0) // 260 0
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  position = Math.max(position - width * count, 
+                      -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
+*/
